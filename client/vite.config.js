04ee2963c8a56d33e4ef3adea2314eaaeb8e2833
vivekrqwat <vivekrawaturfsam@gmail.com>
2026-09-01 22:58:32 +0530

    chnages 02

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,      // Changes dev server port to localhost:3000
+    open: true,      // Automatically opens browser on server start
+  }
 })
